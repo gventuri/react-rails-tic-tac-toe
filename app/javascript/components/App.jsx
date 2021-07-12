d9967b66957f6f6c463ab6348d1ff54fbaf03244
Gabriele <lele.venturi@gmail.com>
2021-07-12 20:21:30 +0200

    story[TTT-5]: add clickable input to allow the user to share the url

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Components
+import Room from "./Room";
 import CreateRoom from "./CreateRoom";
 
-const App = () => <CreateRoom />;
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/rooms/:roomId">
+        <Room />
+      </Route>
+      <Route path="/">
+        <CreateRoom />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
---
+++ app/javascript/components/Room/Room.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { Form, InputGroup, Alert } from "react-bootstrap";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClipboard } from "@fortawesome/free-solid-svg-icons";
+
+const Room = () => {
+  const { roomId } = useParams();
+  const roomUrl = `http://localhost:3000/rooms/${roomId}`;
+  const [copied, setCopied] = useState(false);
+
+  return (
+    <>
+      <Form.Group controlId="validationCustomUsername">
+        <Form.Label>
+          Invite other players (copy link to the clipboard)
+        </Form.Label>
+        <InputGroup>
+          <Form.Control type="text" value={roomUrl} disabled />
+          <InputGroup.Text>
+            <CopyToClipboard
+              text={roomUrl}
+              onCopy={() => {
+                setCopied(true);
+                setTimeout(() => setCopied(false), 3000);
+              }}
+            >
+              <span className="cursor-pointer">
+                <FontAwesomeIcon icon={faClipboard} />
+              </span>
+            </CopyToClipboard>
+          </InputGroup.Text>
+        </InputGroup>
+      </Form.Group>
+
+      <Alert show={copied} variant="success" dismissible>
+        Copied to the clipboard
+      </Alert>
+    </>
+  );
+};
+
+export default Room;
