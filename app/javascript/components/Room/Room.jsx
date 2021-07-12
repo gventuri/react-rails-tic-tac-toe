57bdadcbcc52bb1cfc85db483b65558bb7f32301
Gabriele <lele.venturi@gmail.com>
2021-07-13 00:08:05 +0200

    story[TTT-5]: add board to the room component

--- app/javascript/components/Room/Room.jsx
+++ app/javascript/components/Room/Room.jsx
@@ -4,8 +4,11 @@ import { Form, InputGroup, Alert } from "react-bootstrap";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClipboard } from "@fortawesome/free-solid-svg-icons";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import "./style.scss";
 
-const Room = () => {
+const RoomUrl = () => {
   const { roomId } = useParams();
   const roomUrl = `http://localhost:3000/rooms/${roomId}`;
   const [copied, setCopied] = useState(false);
@@ -39,6 +42,64 @@ const Room = () => {
       </Alert>
     </>
   );
+};
+
+const Cell = ({ symbol, onChange }) => (
+  <div className="col col-4 p-2">
+    <div
+      className={classNames("cell", {
+        [`cell-${symbol}`]: symbol,
+      })}
+      onClick={() => onChange("x")}
+      role="button"
+      tabIndex="0"
+    />
+  </div>
+);
+
+const Room = ({ defaultCells }) => {
+  const [cells, setCells] = useState(defaultCells);
+
+  const updateCell = (key, value) => {
+    setCells({
+      ...cells,
+      [key]: value,
+    });
+  };
+
+  return (
+    <>
+      <RoomUrl />
+
+      <div className="row">
+        {Object.keys(cells).map((key) => (
+          <Cell
+            onChange={(newValue) => updateCell(key, newValue)}
+            symbol={cells[key]}
+            key={key}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+Room.propTypes = {
+  defaultCells: PropTypes.objectOf(PropTypes.any),
+};
+
+Room.defaultProps = {
+  defaultCells: {
+    0: null,
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+    5: null,
+    6: null,
+    7: null,
+    8: null,
+  },
 };
 
 export default Room;
